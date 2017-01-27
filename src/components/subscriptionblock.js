f26a4878520c0be3226afdf902927cbf2882d178
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-27 14:06:37 +0000

    add subscription block content

--- src/components/subscriptionblock.js
+++ src/components/subscriptionblock.js
@@ -5,7 +5,10 @@ class SubscriptionBlock extends Component {
   render() {
     return (
       <div className="subscription-block">
-        Subscription block
+        <h2> Let us help you discover your story </h2>
+        <p> Begin your free family tree with a few simple facts. We'll help you discover a lot more. Start your tree by subscribing today for a free trial.
+        </p>
+        <button>Subscribe Today</button>
       </div>
     );
   }
