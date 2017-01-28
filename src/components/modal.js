407b2cbd720036d3ead21c10972bbf2cc0b9d345
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-28 14:20:43 +0000

    add overlay when modal is open, style modal, WIP

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,7 +4,30 @@ import '.././styles/modules/modal.css';
 class Modal extends Component {
   render() {
     return (
-      <div className="modal"> MODAL </div>
+      <div className="modal__wrapper">
+        <div className="modal__overlay"></div>
+        <div className="modal">
+          <h1>Subscribe</h1>
+          <form>
+            <label className="modal__input-label">
+              Your first name
+              <input className="modal__input-label" type="text" required />
+            </label>
+            <label className="modal__input-label">
+              Your last name
+              <input className="modal__input-label" type="text" required />
+            </label>
+            <label className="modal__input-label">
+              Your email
+              <input className="modal__input-label" type="email" required />
+            </label>
+            <button className="button button--primary">Get started</button>
+            <small>
+              By creating an account, an agree to <a href="#">Ancestry Terms and Conditions</a>
+            </small>
+          </form>
+        </div>
+      </div>
     );
   }
 }
