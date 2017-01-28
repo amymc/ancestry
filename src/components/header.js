ac125d839e93eed2d7769d29861b1efa869b6168
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-28 18:18:52 +0000

    header styles

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,11 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="header__background"></div>
+        <ul className="header__breadcrumbs">
+          <li className="header__breadcrumb">Home</li>
+          <li className="header__breadcrumb">Search</li>
+          <li className="header__breadcrumb">US Military Collection</li>
+        </ul>
         <h1 className="header__title">U.S. Military Collection</h1>
       </header>
     );
