9759f07fe8437f79fe304cc3ab3b5c83cd8988e9
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 00:06:28 +0000

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-//import './index.css';
 import './styles/utilities/reset.css';
 import './styles/base/main.css';
 
